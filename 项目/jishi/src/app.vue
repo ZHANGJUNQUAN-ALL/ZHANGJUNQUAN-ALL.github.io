d9cf0cfc819a0ef6b958567fe171999d8cd08637
nine-song <wangjingtao319@qq.com>
2019-11-21 07:47:22 +0800

    修改

---
+++ 项目/jishi/src/app.vue
@@ -0,0 +1,455 @@
+<template>
+	<div>
+		<div class="title">
+			<div style="width: 90%; height: 466px; margin-left: 80px; padding-top: 100px;">
+				<div class="header_title" style="font-size:60px; text-align: center; font-weight:normal;font-style:normal;color:#ffffff;">起飞页自助建站平台</div>
+				<div class="title_x" style="width: 700px;overflow: hidden;; height: 100px; margin: 50px auto; border-top: 2px solid #CCCCCC; border-bottom: 2px solid #CCCCCC;">
+					<!-- <p>与创业为伴，点亮创新，<span>是云建站的使命</span></p> -->
+
+					<div class="swiper-container" style="overflow: hidden;">
+						<div class="swiper-wrapper">
+							<div class="swiper-slide">与创业为伴,点亮创新,<span>是云建站的使命.</span></div>
+							<div class="swiper-slide">云建站自助平台,<span>成就梦想</span></div>
+
+						</div>
+						<!-- Add Pagination -->
+						<div class="swiper-pagination"></div>
+					</div>
+
+				</div>
+				<div style="text-align: center;"><span style="font-size:26px;"><span style="font-family: 微软雅黑体;"><span style="letter-spacing: 3px;"><span
+								 style="color: rgb(255, 255, 255);"><span style="font-family: 华文细黑;">自助建站平台</span></span>
+							</span>
+						</span>
+					</span>
+				</div>
+				<div class="loci clearfix">
+					<ul>
+						<li><img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvYjgxZjYwZjI5NWFlMzgzODExMzliNDU2ZTkwMGUyYTEtNDd4NDcucG5n.png"
+							 alt="" /></li>
+						<li><img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvODUyMjA2ZWViZDZiMmY0YjU1ZjkwZGI3MjNhODRjNWMtNDd4NDcucG5n.png" /></li>
+						<li><img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvZjU4N2RiMTkzOWE0YjI4NTk0NzA3MjFjYTJhZTYwMDAtNDd4NDcucG5n.png" /></li>
+						<li><img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvOWIxMTg4ODYwZDI4MzE0MTlmNjkyZjJhM2Y5ZTY5OGUtNDd4NDcucG5n.png"
+							 alt="" /></li>
+						<li><img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvZDczNTgwOTlmZWEzN2UyY2E4YTVjZWVjNmIxMDU4NmUtNDd4NDcucG5n.png"
+							 alt="" /></li>
+						<li><img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDcvZjcxMDlkMGU1YmUwZWQ1MjA5ZTk0YjFlYjMwMzY3YjYtNDd4NDcucG5n.png"
+							 alt="" /></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="center">
+			<div class="column_containter" style="z-index:3;position:relative;">
+				<div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-81177 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_top-to-bottom qfe_start_animation"
+				 style="position: relative;;;line-height:3em;;background-repeat: no-repeat;;">
+					<div class="qfe_wrapper">
+						<div style="position: relative;">
+							<div><span style="color: rgb(51, 51, 51); font-family: 微软雅黑, Verdana, Arial, Helvetica, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255); text-align: center; display: block;"></span><span
+								 style="color: rgb(51, 51, 51); font-family: 微软雅黑, Verdana, Arial, Helvetica, sans-serif; font-size: 14px; text-align: center; display: block;"><span
+									 style="line-height:1;"><span style="font-family: 微软雅黑;"><span style="word-wrap: break-word; word-break: break-all; color: rgb(232, 76, 61);"><span
+												 style="word-wrap: break-word; word-break: break-all; font-size: 30px;">您的创业梦想需要一个非同凡响的网站</span></span>
+										</span>
+									</span>
+								</span>
+							</div>
+						</div>
+
+					</div>
+				</div>
+				<div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-42482 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_bottom-to-top qfe_start_animation"
+				 style="position: relative;;;line-height:1.5em;;background-repeat: no-repeat;;margin-top:25px;">
+					<div class="qfe_wrapper">
+						<div>
+							<div style="text-align: center;"><span style="font-family:微软雅黑;"><span style="word-wrap: break-word; word-break: break-all; color: rgb(105, 105, 105);">云建站倾力打造的一个从网站建设.网网站运营管理到网站推广的自动化免费建站平台</span></span>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div style="width: 90%; height: 250px; margin: 50px auto;overflow: hidden;">
+				<div style="width: 30%;height: auto;text-align: center;float: left;">
+					<img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvZjgxMjkwZTg3YTEzM2ZkNWFiM2IzY2ExNjdhNTA3ZDgucG5n.png"
+					 alt="">
+					<p style="font-size: 14px;color: #B2B2B2;padding: 10px 0px;">页面要炫酷,颜值要高</p>
+					<p style="font-size: 12px;color: #B2B2B2;line-height: 20px;">人靠衣装马靠鞍,一个炫酷的网站才配得上创业者的新潮创
+						意.微企点采用HTML5最新技术,拥有大气舒适的界面和众多 精美模板,用起飞页制作一个高逼格.高颜值的炫酷网站, 让您的创业项目脱颖而出,非同凡响.</p>
+
+				</div>
+				<div style="width: 30%;height: auto;text-align: center;margin: 0 auto;">
+					<img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvY2JmYjViODAyM2M4ZjczMzU4YmY0OGIxMmEwNmRjNDkucG5n.png"
+					 alt="">
+					<p style="font-size: 14px;color: #B2B2B2;padding: 10px 0px;">简单易用,随需而变</p>
+					<p style="font-size: 12px;color: #B2B2B2;line-height: 20px;">互联网创业瞬息万变,天下武功,唯快不破.创业者可以按需选择起飞页中精美的网页模板,预置栏目,快速添加和编辑网页无需编程,一步即创建一个炫酷高颜值的网站.网站从无到有,倚马可待.</p>
+
+				</div>
+				<div style="width: 30%;height: auto;text-align: center;float: right;margin-top: -205px;">
+					<img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvMzY3N2Y5N2FhMjQ5YThmNmFlODUxNGJiM2E1NWExZGEucG5n.png"
+					 alt="">
+					<p style="font-size: 14px;color: #B2B2B2;padding: 10px 0px;">支持多终端跨屏展示</p>
+					<p style="font-size: 12px;color: #B2B2B2;line-height: 20px;">在移动互联网全面渗透时,创业者需要一个随屏幕大小不同而自动伸缩的网站,这样同一份内容都可为不同终端用户带来最佳用户体验.微企点使用最新的HTML5响应式技术,网页界面随不同屏幕尺寸自动伸缩.</p>
+
+				</div>
+			</div>
+
+			<div class="bag">
+				<div style="background-color: rgba(5,5,5,0.3);height: 90px;padding-top: 10px;">
+					<div style="text-align: center;">
+						<!-- <p style="color: #CCDAE1;padding: 20px 0;">云建站自助平台</p>
+						<p style="color: rgb(232, 76, 61);padding: 10px 0;">上手快.颜值高.跨多屏.自传播.全免费.</p> -->
+						<div class="qfe_wrapper">
+							<div style="margin: 0px 50px;"><i style="display: block; font-style: normal; text-align: center;"><span style="font-family:微软雅黑;"><span
+										 style="font-size:24px;"><span style="color: rgb(255, 255, 255);"></span></span></span></i><i style="display: block; font-style: normal; text-align: center;"><span
+									 style="font-family:微软雅黑;"><span style="font-size:24px;"><span style="color: rgb(255, 255, 255);">云建站自助平台</span></span></span></i><i
+								 style="display: block; font-style: normal; text-align: center;"><span style="font-family:微软雅黑;"><span style="font-size:24px;"><span
+											 style="color: rgb(232, 76, 61);">上手快.颜值高.跨多屏.自传播.全免费.</span></span></span></i>
+							</div>
+
+						</div>
+					</div>
+				</div>
+				<div style="margin: 0 auto;width: 35%;">
+					<p style="letter-spacing: 2px;line-height: 20px;width: 480px;font-size: 12px;color: rgb(255, 255, 255);text-align: center;padding-top: 50px;">每一个模版都经过设计、制作、测试、改进等多道工序，力求在达到赏心悦目的艺术视觉效果的同时，又容易定制改变。除此以外，多达180个精美的常用设计模式和30余个功能组件让您在自助建站时得心应手，事半功倍。</p>
+				</div>
+			</div>
+
+
+			<div class="jsj">
+				<div>
+					<div class="column_containter" style="z-index:3;position:relative;">
+						<div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-75845 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_top-to-bottom qfe_start_animation"
+						 style="position: relative;;;line-height:2em;;background-repeat: no-repeat;;">
+							<div class="qfe_wrapper">
+								<div style="position: relative;">
+									<div><span style="color: rgb(51, 51, 51); font-family: 微软雅黑, Verdana, Arial, Helvetica, sans-serif; font-size: 14px; text-align: center; display: block;">&nbsp;
+											<span style="font-family:微软雅黑;"> <span style="word-wrap: break-word; word-break: break-all; color: rgb(232, 76, 61);">
+													<span style="word-wrap: break-word; word-break: break-all; font-size: 30px;"> 云建站，一站式的在线H5建站平台</span></span></span></span></div>
+								</div>
+
+							</div>
+						</div>
+						<div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-22594 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_bottom-to-top qfe_start_animation"
+						 style="position: relative;;;line-height:1.5em;;background-repeat: no-repeat;;margin-top:25px;">
+							<div class="qfe_wrapper">
+								<div>
+									<div style="text-align: center;margin-bottom: 30px;"><span style="font-family:微软雅黑;"><span style="word-wrap: break-word; word-break: break-all; color: rgb(105, 105, 105);font-size: 14px;">云建站倾力打造的一个从网站建设.网网站运营管理到网站推广的自动化免费建站平台</span></span></div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div style="margin: 0 auto;width: 700px;height: 600px;">
+					<img style="width: 100%;height: 100%;" src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvNTE1Y2RhYTQxNDg0YWRiZDY4Mjg1NmU0OGNkZjFlYWYucG5n.png"
+					 alt="">
+				</div>
+			</div>
+
+			<div class="obox">
+				<div>
+					<div style="text-align: center;"><span style="font-family:微软雅黑;"> <span style="color: rgb(232, 76, 61);"> <span
+								 style="font-size: 30px;">为创业者而生的免费建站平台</span></span></span></div>
+					<p style="color: rgb(105, 105, 105);text-align: center;font-size: 14px;margin-top: 30px;">用云建站平台,上手快.颜值高.跨多屏.自传播.全免费.</p>
+				</div>
+				<div>
+					<div id="XSDFXPage" class="XSDFXPage">
+					</div>
+				</div>
+			</div>
+
+
+
+		</div>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<script>
+	export default {
+		mounted(){
+			var markerPointLevel1 = [{
+					buildingNum: "123",
+					id: 1,
+					lat: "30.207036169515438",
+					lng: "120.08899292561351",
+					name: "西湖区"
+				},
+				{
+					buildingNum: "79",
+					id: 2,
+					lat: "30.34473201035828",
+					lng: "120.15884493257122",
+					name: "拱墅区"
+				},
+				{
+					buildingNum: "51",
+					id: 3,
+					lat: "30.187587607727094",
+					lng: "120.19237042945677",
+					name: "滨江区"
+				},
+				{
+					buildingNum: "16",
+					id: 6,
+					lat: "30.2323576392334",
+					lng: "120.18012613888587",
+					name: "上城区"
+				}
+			];
+
+			var data = {
+				center: {
+					level: 15,
+					longitude: 120.154571,
+					dimensionality: 30.270801
+				},
+				setting: {
+					cityName: "杭州",
+					enableScrollWheelZoom: true,
+					imgSize: {
+						x: 20,
+						y: 50
+					},
+					param: {
+						//说明窗口
+						width: 5,
+						height: 3,
+						title: "高压塔3"
+					}
+				}
+			}
+			var map = new BMap.Map("XSDFXPage", {
+				enableMapClick: true
+			});
+			const point = new BMap.Point(
+				data.center.longitude,
+				data.center.dimensionality
+			);
+			map.centerAndZoom(point, data.center.level);
+			// 添加地图类型控件
+			map.addControl(new BMap.MapTypeControl());
+			// 设置地图显示的城市 此项是必须设置的
+			map.setCurrentCity("杭州");
+			// 开启鼠标滚轮缩放
+			map.enableScrollWheelZoom(true);
+
+			/************************************************
+			 添加工具条、比例尺控件
+			 *************************************************/
+			map.addControl(new BMap.ScaleControl({
+				anchor: BMAP_ANCHOR_TOP_LEFT
+			}));
+			map.addControl(new BMap.NavigationControl());
+			map.addControl(
+				new BMap.NavigationControl({
+					anchor: BMAP_ANCHOR_BOTTOM_RIGHT,
+					type: BMAP_NAVIGATION_CONTROL_SMALL
+				})
+			);
+			/************************************************/
+
+
+
+			/*覆盖物1级层 zoom<11的时候展示
+			 --------------------------------*/
+			function ComplexCustomOverlay1(point, nums, area, districtId) {
+				this._point = point;
+				this._nums = nums;
+				this._area = area;
+				this._districtId = districtId;
+			}
+			ComplexCustomOverlay1.prototype = new BMap.Overlay();
+
+			ComplexCustomOverlay1.prototype.initialize = function(map) {
+				this._map = map;
+				var div = (this._div = document.createElement("div"));
+				var point = this._point;
+				div.style.position = "absolute";
+				div.style.zIndex = BMap.Overlay.getZIndex(this._point.lat);
+				div.style.backgroundColor = "rgba(234,41,60,0.8)";
+				div.style.color = "#fff";
+				div.style.height = "80px";
+				div.style.width = "80px";
+				div.style.borderRadius = "100%";
+				div.style.boxSizing = "content-box";
+				div.style.whiteSpace = "nowrap";
+				div.style.MozUserSelect = "none";
+				div.style.fontSize = "12px";
+				div.style.textAlign = "center";
+				div.style.cursor = "pointer";
+
+				//区
+				var areaDom = (this._areaDom = document.createElement("div"));
+				areaDom.appendChild(document.createTextNode(this._area));
+				areaDom.style.height = "40px";
+				areaDom.style.lineHeight = "60px";
+				areaDom.style.fontSize = "14px";
+				div.appendChild(areaDom);
+
+				//楼盘数量
+				var numsDom = (this._numsDom = document.createElement("div"));
+				numsDom.appendChild(document.createTextNode(this._nums + "个楼盘"));
+				numsDom.style.height = "40px";
+				numsDom.style.lineHeight = "20px";
+				numsDom.style.boxSizing = "content-box";
+				numsDom.style.fontSize = "14px";
+				div.appendChild(numsDom);
+
+				var that = this;
+
+				div.onmouseover = function() {
+					this.style.zIndex = 99999;
+					this.style.backgroundColor = "rgba(234,41,60,1)";
+				};
+				div.onmouseout = function() {
+					this.style.zIndex = BMap.Overlay.getZIndex(point.lat);
+					this.style.backgroundColor = "rgba(234,41,60,0.8)";
+				};
+				div.onclick = function() {
+					filterObj.districtId = that._districtId;
+					map.centerAndZoom(point, 14); //跳到设置点并设置zoom为14级
+					$(".l-select.district").find(".result")[0].childNodes[0].textContent =
+						that._area; //点击区域,相应左侧区域文字要同步
+				};
+				map.getPanes().labelPane.appendChild(div);
+
+				return div;
+			};
+
+			ComplexCustomOverlay1.prototype.draw = function() {
+				var map = this._map;
+				var pixel = map.pointToOverlayPixel(this._point);
+				this._div.style.left = pixel.x - 40 + "px";
+				this._div.style.top = pixel.y - 40 + "px";
+			}
+
+			function ComplexCustomOverlay1(point, nums, area, districtId) {
+				this._point = point;
+				this._nums = nums;
+				this._area = area;
+				this._districtId = districtId;
+			}
+
+			// 编写自定义函数,创建标注(1级)
+			function addMarkerLevel1(point, nums, area, districtId) {
+				// var marker = new BMap.Marker(point);
+				// map.addOverlay(marker);
+				var myCompOverlay = new ComplexCustomOverlay1(
+					point,
+					nums,
+					area,
+					districtId
+				);
+				map.addOverlay(myCompOverlay);
+			}
+
+			map.clearOverlays();
+			for (let i = 1; i < markerPointLevel1.length; i++) {
+				let point = new BMap.Point(
+					markerPointLevel1[i].lng,
+					markerPointLevel1[i].lat
+				);
+				addMarkerLevel1(
+					point,
+					markerPointLevel1[i].buildingNum,
+					markerPointLevel1[i].name,
+					markerPointLevel1[i].id
+				);
+			}
+		}
+	}
+</script>
+
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+		text-decoration: none;
+	}
+
+	.center {
+		height: 460px;
+		/* background: #CCCCCC; */
+		padding-top: 90px;
+	}
+
+	.bag {
+		height: 300px;
+		background-image: url('https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvMWYyYjI0N2JjYTc5MjhkYzIyOTRiMjc0M2UyZDI1MWQuanBn.jpg');
+		background-size: 100% 100%;
+	}
+
+	.clearfix:after,
+	.clearfix:before {
+		content: "";
+		display: table;
+	}
+
+	.clearfix:after {
+		clear: both;
+	}
+
+	.clearfix {
+		*zoom: 1;
+	}
+
+	.title {
+		height: 653px;
+		background: url('https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDMvNzIzNGQ1MjBkZjBiODhiMDIwYTc3ZmYwZjk1MGM3OGEuanBn.jpg');
+		background-size: 100% 100%;
+		padding-top: 50px;
+	}
+
+	.title_x p {
+		font-size: 30px;
+		color: white;
+		line-height: 100px;
+		text-align: center;
+	}
+
+	.title_x span {
+		display: inline-block;
+		font-size: 30px;
+		color: red;
+	}
+
+	.loci {
+		width: 320px;
+		height: 50px;
+		margin: 25px auto;
+	}
+
+	.loci ul li {
+		float: left;
+		margin-left: 5px;
+	}
+
+	.jsj {
+		height: 700px;
+		padding-top: 40px;
+	}
+
+	.swiper-slide {
+		color: #F0F0F0;
+		height: 100px;
+		font-size: 35px;
+		text-align: center;
+		line-height: 100px;
+	}
+
+	.obox {
+		margin-top: 50px;
+	}
+
+	.XSDFXPage {
+		width: 1200px;
+		height: 500px;
+		margin: 100px auto;
+	}
+</style>
